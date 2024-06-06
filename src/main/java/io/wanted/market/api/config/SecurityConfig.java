4c6d3297e7d3e811dfd9730bdcc9468185b29de8
YoungJun-L <dudwns328@naver.com>
2024-06-07 00:23:36 +0900

    feat: security config 설정

---
+++ src/main/java/io/wanted/market/api/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package io.wanted.market.api.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+    private static final String[] ALLOWED_URLS = {
+            "/auth/login",
+            "/auth/register",
+            "/auth/token",
+            "/health"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(ALLOWED_URLS).permitAll()
+                        .anyRequest().anonymous()
+                )
+                .requestCache(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @ConditionalOnProperty("spring.h2.console.enabled")
+    public WebSecurityCustomizer webSecurityCustomizerH2Enabled() {
+        return web -> web.ignoring().requestMatchers(PathRequest.toH2Console());
+    }
+}
