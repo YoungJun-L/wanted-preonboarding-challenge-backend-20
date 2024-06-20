7d2b0587b327bb404d6edf784ae63317f5ea37a5
YoungJun-L <dudwns328@naver.com>
2024-06-20 15:21:01 +0900

    refactor: core errorCode 제거

--- src/main/java/io/wanted/market/core/domain/support/error/CoreErrorCode.java
+++ src/main/java/io/wanted/market/core/domain/support/error/CoreErrorCode.java
@@ -1,5 +1,5 @@
 package io.wanted.market.core.domain.support.error;
 
 public enum CoreErrorCode {
-    E4000, E4030, E4040, E4041, E4001, E4031, E4002, E4010, E4011, E4012, E5000
+    E4000, E4030, E4040, E5000
 }
